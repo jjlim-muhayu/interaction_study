40dd9497940db26d801d3ae0129602cf77151b95
jongjin7 <ezcode2875@gmail.com>
2023-07-31 02:37:19 +0900

    #THREE.js part3. 인터랙티브 카드 - r값이 있는 카드 생성을 위하여 ShapeGeometry로 변경 - Shpe인스턴스 사용

--- interactive-fastcamp/three-interactive/3-interactive-card-jong/src/Card.js
+++ interactive-fastcamp/three-interactive/3-interactive-card-jong/src/Card.js
@@ -1,8 +1,20 @@
 import * as THREE from "three";
 
 class Card{
-    constructor({width, height, color}){
-        const geometry = new THREE.PlaneGeometry(width, height)
+    constructor({width, height, radius, color}){
+        // x,y축의 중심을 정의한다.
+        const x = width /2 - radius
+        const y = height /2 - radius
+        const shape = new THREE.Shape()
+        shape
+            .absarc(x, y, radius,Math.PI / 2, 0, true)
+            .lineTo(x + radius, -y)
+            .absarc(x, -y, radius, 0,-Math.PI / 2, true)
+            .lineTo(-x, -(y + radius))
+            .absarc(-x, -y, radius,-Math.PI / 2, Math.PI, true)
+            .lineTo(-(x+radius), y)
+            .absarc(-x, y, radius,Math.PI, Math.PI / 2,true)
+        const geometry = new THREE.ShapeGeometry(shape)
         const material = new THREE.MeshStandardMaterial({
              color,
             side: THREE.DoubleSide, // 앞뒤 양면 모두에 메터리얼 적용
