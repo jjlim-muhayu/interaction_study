bfa2926ed360177dd62f7017fa796774fc2bc129
jongjin7 <ezcode2875@gmail.com>
2023-08-10 02:19:17 +0900

    #THREE.js part5. fireworks-파티클 애니메이션 - 3.BufferGeometry함수를 사용하여 정점을 랜덤하게 생성

--- interactive-fastcamp/three-interactive/5-fireworks-jong/src/main.js
+++ interactive-fastcamp/three-interactive/5-fireworks-jong/src/main.js
@@ -27,11 +27,28 @@ function init() {
 
     new OrbitControls(camera, renderer.domElement)
 
-    const geometry = new THREE.SphereGeometry();
+    const geometry = new THREE.BufferGeometry();
+    const count = 10000;
+    const positions = new Float32Array(count * 3) //정점 생성 갯수 * 3(x,y,z)
+    // 화면중심으로 특정한 크기의 정육면체안에 점점들을 랜덤하게 생성한다.
+    for (let i = 0; i < count; i++) {
+        // 기본 랜덤값 정의: 0~1
+        // position[i * 3] = Math.random() - 0.5
+        // position[i * 3 + 1] = Math.random() - 0.5
+        // position[i * 3 + 2] = Math.random() - 0.5
+
+        //THREE.js의 내장객체를 이용한 랜덤값 정의
+        positions[i * 3] = THREE.MathUtils.randFloatSpread(1) //x좌료, 1: -0.5~0.5
+        positions[i * 3 + 1] = THREE.MathUtils.randFloatSpread(1) //y좌표
+        positions[i * 3 + 2] = THREE.MathUtils.randFloatSpread(10) //z좌표
+    }
+    //버퍼위치 정보를 담는다.
+    // BufferAttribute에 정점들을 등록한다.
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) //마지막 인자 3은 점 한개에 대한 좌표의 집합(x,y,z) 의미함
     const material = new THREE.PointsMaterial(
         {
             color: 'pink',
-            size: 10,
+            size: 1,
             sizeAttenuation: false, // 원근에 따른 점의 크기 차이를 사용하지, 말지...
         }
     )
